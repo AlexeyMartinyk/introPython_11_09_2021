a8d0a22cc2fd778f1107e366eeb67c24e7e101fe
AlexeyMartinyk <90513420+AlexeyMartinyk@users.noreply.github.com>
2021-10-07 12:45:07 +0200

    Add files via upload

---
+++ Дз_3.py
@@ -0,0 +1,33 @@
+#
+# 1.
+# my_list = [2, 4, 110, 8, 100, 99, -2]
+# for symbol in my_list:
+#     if symbol >= 100:
+#         print(symbol)
+# 2.
+# my_list = [2, 4, 110, 8, 100, 99, 192, -2]
+# my_result = []
+# for symbol in my_list:
+#     if symbol >= 100:
+#         my_result.append(symbol)
+# print(my_result)
+# 3.
+# my_list = [2, 4, 110, 8, 100, 99, 192, -2]
+# if len(my_list) < 2:
+#     my_list.append(0)
+# else:
+#     len(my_list) >= 2
+#     res = sum(my_list[-2: None])
+#     print(res)
+#
+#3/
+# my_string_1 = "0123456789"
+# my_string_2 = my_string_1
+# my_list = []
+# for symb_1 in my_string_1:
+#     for symb_2 in my_string_2:
+#         my_list.append(int(symb_1+symb_2))
+# print(my_list)
+
+
+
