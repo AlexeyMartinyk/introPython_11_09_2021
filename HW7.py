094a6bcf777d25d8b6424b136f7b88fed0b2df4f
AlexeyMartinyk <90513420+AlexeyMartinyk@users.noreply.github.com>
2021-11-19 11:24:53 +0200

    Update and rename HW7 to HW7.py

--- HW7.py
+++ HW7.py
@@ -4,7 +4,7 @@ import json
 import re
 from operator import itemgetter
 
-os.chdir("HW")
+os.chdir("HW_DOCS")
 
 
 # 1. Необходимо написать функцию,
@@ -17,7 +17,7 @@ def read_json(filename):
         return data
 
 
-print(read_json("data_orig.json"))
+print(read_json("data.json"))
 
 
 # 2. Написать функцию сортировки данных по ФАМИЛИИ в поле "name" (у тех у кого она есть).
@@ -30,12 +30,12 @@ def sort_by_surname(data):
         return sort_name
 
 
-print(sort_by_surname("data_orig.json"))
+print(sort_by_surname("data.jsonn"))
 
 # 3. Написать функцию сортировки по дате смерти из поля "years".
 # Обратите внимание на сокращение BC. - это означает до н.э.
 
-dict_list = read_json("data_orig.json")
+dict_list = read_json("data.json")
 
 
 def key_sorted_by_data(obj_dict):
@@ -55,5 +55,5 @@ def sort_by_len_text(person_dict):
     return len(person_dict["text"].split())
 
 
-data_math_sort_by_len_text = sorted(read_json("data_orig.json"), key=lambda person_dict: len(person_dict["text"].split()))
+data_math_sort_by_len_text = sorted(read_json("data.json"), key=lambda person_dict: len(person_dict["text"].split()))
 print(data_math_sort_by_len_text)
