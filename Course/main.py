35a8e6bcae396c552a56ac2e1b586688c71bd056
AlexeyMartinyk <90513420+AlexeyMartinyk@users.noreply.github.com>
2021-12-04 14:56:22 +0200

    Update main.py

--- Course/main.py
+++ Course/main.py
@@ -1,37 +1,36 @@
 import sys
-import trader
 import os
-
+import trader
 name_code, param_console = sys.argv
 
 def main():
     if param_console == "RESTART":
-        def_code.restart_param()
+        trader.restart_param()
         print("The game is restarted, enter INFO to output commands")
     elif param_console == "NEXT":
         if os.stat("sys.json").st_size == 0:
             print("Enter RESTART")
         else:
-            print("Course updated:", def_code.next_param())
-            def_code.write_file_sys()
+            print("Course updated:", trader.next_param())
+            trader.write_file_sys()
     elif param_console == "RATE":
-        print(def_code.rate_param())
+        print(trader.rate_param())
     elif param_console == "BUY":
-        def_code.buy_param()
-        def_code.write_file_sys()
+        trader.buy_param()
+        trader.write_file_sys()
     elif param_console == "BUY_UAH":
-        def_code.buy_uah_param()
-        def_code.write_file_sys()
+        trader.buy_uah_param()
+        trader.write_file_sys()
     elif param_console == "BUY_ALL":
-        def_code.buy_all_param()
-        def_code.write_file_sys()
+        trader.buy_all_param()
+        trader.write_file_sys()
     elif param_console == "AVAILABLE":
-        def_code.available_param()
+        trader.available_param()
     elif param_console == "SELL_ALL":
-        def_code.sell_all_param()
-        def_code.write_file_sys()
+        trader.sell_all_param()
+        trader.write_file_sys()
     elif param_console == "INFO":
-        def_code.info_param()
+        trader.info_param()
     else: print("There is no such command, enter INFO")
 
 sys.exit(main()) if __name__ == '__main__' else print("error")
